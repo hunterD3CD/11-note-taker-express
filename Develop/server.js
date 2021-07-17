fcf5caee25aa765b5bf034179bd604ac12fd0eb5
hunterD3CD <hunterd3c@gmail.com>
2021-07-17 16:31:46 -0700

    Sets up the Express app to handle data parsing

--- Develop/server.js
+++ Develop/server.js
@@ -8,3 +8,8 @@ const fs = require("fs");
 // --------------------database file and port connection-------------------
 const PORT = 3000;
 const database = require("./db/db");
+// -------------------link to the public folder----------------------------
+app.use(express.static("public"));
+// --------------------Sets up the Express app to handle data parsing-------
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
