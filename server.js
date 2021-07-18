12bc8c96f437742d6ada4c8411f23879f40e4541
hunterD3CD <hunterd3c@gmail.com>
2021-07-18 12:05:46 -0700

    file location

---
+++ server.js
@@ -0,0 +1,33 @@
+// Dependencies
+// =============================================================
+// --------------------node modules: express, path, and fs----------------
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+// --------------------database file and port connection-------------------
+const PORT = process.env.PORT || 3000;
+
+const database = require("./db/db");
+// -------------------link to the public folder----------------------------
+app.use(express.static("public"));
+// --------------------Sets up the Express app to handle data parsing-------
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+// =============================================================
+// -----------------display html file on the web----------------------------
+app.get("/", function (req, res) {
+  // transfer the file at the given path
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+// Listener
+// =============================================================
+app.listen(PORT, () => {
+  console.log(`App listening on PORT http://localhost:${PORT}`);
+});
